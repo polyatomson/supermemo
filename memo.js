bf48372980d707e6a1650b189649b5427b4c7ed1
Polya Tomson <polyatomson@gmail.com>
2024-01-06 18:49:20 +0100

    Drag and drop works

--- memo.js
+++ memo.js
@@ -1,66 +1,59 @@
-// let colors = ["white", "yellow", "blue",
-//           "green", "brown", "black", "red", "orange"];
-
-// console.log(colors)
 document.addEventListener('DOMContentLoaded', (event) => {
 
 function handleDragStart(e) {
-    console.log('event', e)
-    console.log('this', this)
-    dragSrcEl = this
-    console.log('started dragging', this)
-    if (this.classList[1] === undefined) {
-        // console.log(this.classList)
-        console.log("second class", this.classList[1]);
-        // console.log("list of classes", this.classList[0]);
-        console.log('new color');
-        dragSrcEl.classList.add('dropped');
-        dragSrcEl = dragSrcEl.outerHTML;
-        e.dataTransfer.effectAllowed = 'copy';
-        e.dataTransfer.setData('text/plain', this.outerHTML);
-    }
-    else {
-        console.log('moving dropped', dragSrcEl.innerHTML)
-        dragSrcEl = dragSrcEl.innerHTML;
-        e.dataTransfer.effectAllowed = 'move';
-        e.dataTransfer.setData('text/plain', dragSrcEl);
-
-    }
+    e.dataTransfer.items.add(e.target.classList, "text_class");
+    console.log("moving from", this.id)
+    e.dataTransfer.items.add(this.id, "text_id");
+    
 }
 
-function handleDragEnd(e) {
-    
+function handleDragEnd(e) {    
   };
 
   function handleDragOver(e) {
     e.preventDefault();
+    this.classList.add('over');
     return false;
   }
 
 function handleDragEnter(e) {
-    // console.log(this);
+    // console.log(e);
     // this.classList.add('over');
   }
 
 function handleDragLeave(e) {
-    // this.classList.remove('over');
+    this.classList.remove('over');
   }
 
 function handleDrop(e) {
     e.stopPropagation(); // stops the browser from redirecting.
-    console.log("New", dragSrcEl)
-    dragSrcEl = this.children[1]
-    console.log("Old", dragSrcEl)
-    this.innerHTML = e.dataTransfer.getData('text/plain');
-    this.classList.add('filled');
-
-    return false;
+    this.classList.remove('over');
+    var new_class = e.dataTransfer.getData("text_class");
+    var previous_id = e.dataTransfer.getData("text_id");
+    console.log("previous_id", previous_id)
+    if (e.target.classList[0] == "marble") {
+        if (previous_id !== '') {
+            var previous_cell = document.getElementById(previous_id)
+            console.log("previous cell", previous_cell)
+            previous_cell.children[0].classList = e.target.classList
+         }
+        e.target.classList = new_class;
+        e.target.draggable = true;
+    }
+    else {
+        if (previous_id !== '') {
+            var previous_cell = document.getElementById(previous_id)
+            console.log("previous cell", previous_cell)
+            previous_cell.children[0].classList = e.target.children[0].classList
+         }
+        e.target.children[0].classList = new_class
+        e.target.children[0].draggable = true;
+    }
   };
   
-let colors = document.querySelectorAll('.col img.colors');
+let colors = document.querySelectorAll('.colors');
     console.log(colors)
     colors.forEach(function (color) {
-        
         color.addEventListener('dragstart', handleDragStart);
         color.addEventListener('dragleave', handleDragLeave);
         color.addEventListener('dragend', handleDragEnd);
@@ -68,9 +61,7 @@ let colors = document.querySelectorAll('.col img.colors');
   });
 
   let positions = document.querySelectorAll('td.positions');
-    positions.forEach(function(el) {
-        // console.log(el.classList[1])
-    });
+    console.log(positions)
     positions.forEach(function (position) {
         position.addEventListener('dragstart', handleDragStart);
         position.addEventListener('dragover', handleDragOver);
